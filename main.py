d795fd9b925f644c230dfbf997add6af4f93c9cf
Potalej <66756864+Potalej@users.noreply.github.com>
2022-06-09 22:25:44 -0300

    Main com exemplo
    Adicionado um arquivo main.py rodando um exemplo.

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+    Para rodar os exemplos, devido às questões de diretório do Python, é bom rodar este arquivo. No entanto, para rodar os exemplos diretamente basta pô-los na mesma pasta que o `diferencas_finitas.py` e o `resolver.py`.
+"""
+
+# rodando o exemplo 3
+import exemplos.exemplo3
